4a1de408d6286e8f58cc59024b37311891de4d4b
jlopesil <jonathan.leoesdejuda@gmail.com>
2022-09-23 15:39:21 -0300

    Criação dos testes para os demais cenarios - ReajusteService e classe Desempenho

--- Jonathan/Projetos/Teste_JUnit_Calculadora/src/main/java/Modelo/Desempenho.java
+++ Jonathan/Projetos/Teste_JUnit_Calculadora/src/main/java/Modelo/Desempenho.java
@@ -1,6 +1,29 @@
 package Modelo;
 
+import java.math.BigDecimal;
+
 public enum Desempenho {
 
-  A_DESEJAR;
+  A_DESEJAR{
+    @Override
+    public BigDecimal percetualReajuste() {
+      return new BigDecimal("0.03");
+    }
+
+  },
+  BOM {
+    @Override
+    public BigDecimal percetualReajuste() {
+      return new BigDecimal("0.15");
+    }
+  },
+  OTIMO {
+    @Override
+    public BigDecimal percetualReajuste() {
+      return new BigDecimal("0.20");
+    }
+  };
+
+  public abstract BigDecimal percetualReajuste();
+
 }
